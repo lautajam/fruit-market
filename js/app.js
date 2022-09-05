85c69352563be2de6c235dd12a37c8803603b76c
lautajam <lautajamdev@gmail.com>
2022-09-05 17:37:09 -0300

    I changed the way of saving and displaying the products
    now i use an array and array methods to save the selected products and display the products by looping through the array

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ const template = document.querySelector("#template");
 const fragment = document.createDocumentFragment();
 const btns = document.querySelectorAll(".btn");
 
-const ObjectCart = {};
+const ArrayCart = [];
 
 const addToCart = (e) => {
     console.log(e.target.dataset.fruit);
@@ -15,19 +15,25 @@ const addToCart = (e) => {
         quantity: 1
     }
 
-    if (ObjectCart.hasOwnProperty(product.id)) {
-        product.quantity = ObjectCart[product.title].quantity + 1;
-    }
-    ObjectCart[product.title] = product;
+   const index = ArrayCart.findIndex(
+    (item) => item.id === product.id
+   )
+    console.log(index);
 
-    printCart(product);
+    if (index === -1) {
+     ArrayCart.push(product);
+    } else {
+        ArrayCart[index].quantity ++;
+    }
+    console.log(ArrayCart)
+    printCart(ArrayCart);
 }
 
-const printCart = (product) => {
+const printCart = (array) => {
 
     cart.textContent = "";
 
-    Object.values(ObjectCart).forEach(item => {
+    array.forEach(item => {
         const clone = template.content.firstElementChild.cloneNode(true);
         clone.querySelector(".lead").textContent = item.title;
         clone.querySelector(".badge").textContent = item.quantity;
